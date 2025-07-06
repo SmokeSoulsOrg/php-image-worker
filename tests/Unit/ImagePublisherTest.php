c9acf5c5e33286beac5cfc27c75d82c21cfec510
Elias Papa <elias.papa92@gmail.com>
2025-07-06 22:29:44 +0300

    Trying to fix image-update and image-update-dead queues

--- tests/Unit/ImagePublisherTest.php
+++ tests/Unit/ImagePublisherTest.php
@@ -31,13 +31,28 @@ class ImagePublisherTest extends TestCase
         Config::set('services.rabbitmq.image_update_queue', 'image-update');
 
         $mockChannel = Mockery::mock(AMQPChannel::class);
-        $mockChannel->shouldReceive('queue_declare')->once()->with('image-update', false, true, false, false);
-        $mockChannel->shouldReceive('basic_publish')->once()->with(Mockery::on(function ($msg) use ($payload) {
-            $decoded = json_decode($msg->getBody(), true);
-            return $decoded == $payload;
-        }), '', 'image-update');
+        $mockChannel->shouldReceive('queue_declare')->once()->with(
+            'image-update',
+            false,
+            true,
+            false,
+            false,
+            false,
+            [
+                'x-dead-letter-exchange'    => ['S', ''],
+                'x-dead-letter-routing-key' => ['S', 'image-update-dead'],
+            ]
+        );
 
-        // Inject mock channel
+        $mockChannel->shouldReceive('basic_publish')->once()->with(
+            Mockery::on(function ($msg) use ($payload) {
+                $decoded = json_decode($msg->getBody(), true);
+                return $decoded == $payload;
+            }),
+            '',
+            'image-update'
+        );
+
         $publisher = new ImagePublisher($mockChannel);
         $publisher->publish($payload);
 
