50cef38e461d7e42e5f27c7505da474aeae719c9
Elias Papa <elias.papa92@gmail.com>
2025-07-06 13:22:43 +0300

    Fixed tests after last change

--- tests/Unit/DownloadAndPublishImageTest.php
+++ tests/Unit/DownloadAndPublishImageTest.php
@@ -77,7 +77,7 @@ class DownloadAndPublishImageTest extends TestCase
         $publisher->shouldReceive('publish')
             ->once()
             ->with(Mockery::on(function ($payload) use ($url) {
-                return Str::startsWith($payload['local_path'], 'pornstar-images/')
+                return Str::startsWith($payload['local_path'], 'storage/pornstar-images/')
                     && $payload['url'] === $url;
             }));
         $this->app->instance(ImagePublisher::class, $publisher);
@@ -89,7 +89,7 @@ class DownloadAndPublishImageTest extends TestCase
         $job = new DownloadAndPublishImage(['url' => $url]);
         $job->handle();
 
-        $expectedFilename = 'pornstar-images/' . md5($url) . '.jpg';
+        $expectedFilename = 'storage/pornstar-images/' . md5($url) . '.jpg';
         Storage::disk('public')->assertExists($expectedFilename);
 
         $logSpy->shouldHaveReceived('info')
